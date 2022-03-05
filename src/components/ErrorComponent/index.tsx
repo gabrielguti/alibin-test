052081cce4aa55f8dafe38b03219d689a89e6df5
Gabriel Gutierez <gabriel@gabriel-inspiron-14-3467>
2022-03-05 15:39:28 -0300

    feat: updated logic error and move error component

---
+++ src/components/ErrorComponent/index.tsx
@@ -0,0 +1,23 @@
+import Error from "../../assets/svg/warning.svg";
+import "./style.css";
+
+const ErrorCard = () => {
+  return (
+    <div className="error-box">
+      <div className="error-info">
+        <img src={Error} alt="error-system" />
+        <h1>Ops! Algo deu errado :/</h1>
+        <p>Não foi possível acessar os dados. Tente novamente mais tarde.</p>
+        <p>Possíveis problemas:</p>
+        <ul>
+          <li>Base de dados fora do ar;</li>
+          <li>Navegador instável;</li>
+          <li>Problema interno da aplicação;</li>
+        </ul>
+        <button onClick={() => window.location.reload()}>Recarregar</button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorCard;
